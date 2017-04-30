2215f93073cbadba64ea9410c6c0b3ee925299b7
Nikos Sofras <sofrasnikos@hotmail.com>
2017-04-30 17:47:33 +0300

    dokimastika gia to matplotlib

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,2 +1,31 @@
+%matplotlib inline
+
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+
+# the histogram of the data
+plt.hist(df["Age (years)"], facecolor='green')
+plt.xlabel('Age')
+plt.ylabel('# of Applicants')
+plt.show()
+
 if __name__ == "__main__":
-    print("hello world")
+    df = pd.read_csv('./Documentation/train_set.csv', sep='\t')
+    # for column in df.columns:
+    #     print(column)
+    # print(df.describe())
+    # print(df[["RowNum", "Content", "Category"]].iloc[5:10])
+    # print(df[df["RowNum"] < 5])
+
+    A = np.array(df)
+    print(A.shape)
+
+    for i in range(A.shape[0]):
+        text = ""
+        for j in range(A.shape[1]):
+            text += str(A[i, j]) + ","
+        print(text)
